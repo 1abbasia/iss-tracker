516eb39130f473ce43ba02729844940bf85de3d0
stefnotadevyet <srjack91@gmail.com>
2021-11-13 23:20:49 -0500

    deleted boiler plate and created Map component

---
+++ iss-tracker/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Map from './Map';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>ISS Tracker</h3>
+        <Map />
+      </header>
+    </div>
+  );
+}
+
+export default App;
