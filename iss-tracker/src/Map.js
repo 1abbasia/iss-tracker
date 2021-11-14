bd4be3bbfe5d29d90c506c8e153807a9de0024ad
stefnotadevyet <srjack91@gmail.com>
2021-11-14 02:31:06 -0500

    imported google maps react module

--- iss-tracker/src/Map.js
+++ iss-tracker/src/Map.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react'
 
 class Map extends React.Component {
+
+
 
   state = {
 
   }
 
   render() {
+    const ISS_URL = "http://api.open-notify.org/iss-now.json"
     return (
       <div>
-        this is the map
+        <GoogleMapReact>
+          MAP_PLACEHOLDER
+        </GoogleMapReact>
       </div>
     )
   }
